664d3042b4d3a181a28b3851e5e3ca824306993f
Mathias Desloges <freakdev@gmail.com>
2016-06-17 18:12:40 +0200

    add side menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { triggerAction } from '../reducers';
+import { actions } from '../reducers/sidemenu';
+
+import SideMenu from './partials/SideMenu';
+
+var layoutStyle = {
+  position: "absolute",
+  top: "0",
+  left: "0",
+  width: "100%",
+  height: "100%"  
+}
+
 export default class Layout extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
 
   static contextTypes = {
     router: React.PropTypes.object.isRequired,
@@ -9,7 +26,8 @@ export default class Layout extends React.Component {
 
   render() {
     return (
-      <div>
+      <div style={ layoutStyle } className="layout">
+        <SideMenu />
         { this.props.children }
       </div>
     )
