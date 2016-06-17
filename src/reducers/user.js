6b402eba22a2c96f5c975e141ace6279c94c8c87
Mathias Desloges <freakdev@gmail.com>
2016-06-17 15:28:47 +0200

    implement autologin

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -3,13 +3,22 @@ import { combineReducers } from 'redux';
 
 import loginActions from './loginActions';
 
+import store from 'store';
+
+
 export default combineReducers({
     "infos": (state = {login: "", token: ""}, action) => {
+
+        let userStoredInfos = store.has('user') ? store.get('user') : false;
+
         switch (action.type) {
         case loginActions.LOGIN_SUCCESSFUL:
-          return { login: action.payload.user.login, token: action.payload.token, id: action.payload.user.id };
+            let userData = { login: action.payload.user.login, token: action.payload.token, id: action.payload.user.id };
+            store.set("user", userData);
+            return userData;
         default:
-          return state;
+            var defaultState = userStoredInfos || state;
+            return defaultState;
         }
     }
 });
