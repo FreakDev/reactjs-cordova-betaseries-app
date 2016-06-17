f39f9dff2550b8419a5f61dc244a936cec5de5b9
Mathias Desloges <freakdev@gmail.com>
2016-06-18 01:26:00 +0200

    implement navigation in side-menu

--- src/components/partials/SideMenu.js
+++ src/components/partials/SideMenu.js
@@ -4,22 +4,37 @@ import { connect } from 'react-redux';
 import { triggerAction } from '../../reducers';
 import { actions } from '../../reducers/sidemenu';
 
-var menuWrapperRefStyle = {
+import { Link } from 'react-router';
+
+const menuWrapperRefStyle = {
   position: "absolute",
   top: "0",
-  left: "-95%",
+  left: "-98%",
   width: "100%",
   height: "100%",
-  zIndex: 10
+  zIndex: 10,
+  transition: "background-color 0.1s",
 }
 
-var sideMenuRefStyle = {
+const sideMenuRefStyle = {
   position: "relative",
-  width: "80%",
+  width: "65%",
   height: "100%",
-  left:"-80%",
+  left:"-65%",
   backgroundColor: "white",
   transition: "all 0.7s",
+}
+
+const sideNavStyle = {
+  left: "105%",
+  position: "relative",
+  boxShadow: "none",
+  width: "100%"
+}
+
+
+const itemStyle = {
+  width: "100%"
 }
 
 class Layout extends React.Component {
@@ -34,13 +49,20 @@ class Layout extends React.Component {
 
   render() {
 
-    var menuWrapperStyle = Object.assign({}, menuWrapperRefStyle, { left: this.props.sideMenuOpen ? "0" : "-95%", backgroundColor: this.props.sideMenuOpen ? "rgba(0,0,0,0.7)" : ""});
-    var sideMenuStyle = Object.assign({}, sideMenuRefStyle, { left: this.props.sideMenuOpen ? "0" : "-80%"});
+    var menuWrapperStyle = Object.assign({}, menuWrapperRefStyle, { left: this.props.sideMenuOpen ? "0" : "-98%", backgroundColor: this.props.sideMenuOpen ? "rgba(0,0,0,0.7)" : ""});
+    var sideMenuStyle = Object.assign({}, sideMenuRefStyle, { left: this.props.sideMenuOpen ? "0" : "-65%"});
 
+    let id = 0;
     return (
-      <div onClick={ this.props.onClick } style={ menuWrapperStyle } className="side-menu">
+      <div onClick={ this.props.onClick } style={ menuWrapperStyle } className="side-panel">
         <div style={ sideMenuStyle }>
-            { this.props.children }
+          <ul className="side-nav" style={ sideNavStyle }>
+            { this.props.items.map((item) => (
+              <li style={itemStyle} key={ id++ } className="no-padding waves-effect waves-teal">
+                <Link style={ itemStyle } to={"/" + item.toLowerCase()}>{item}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     )
